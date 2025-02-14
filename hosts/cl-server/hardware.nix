16ee011006706166147e215923a32dabd436125e
chlorodose <chlorodose@chlorodose.me>
2025-02-14 18:49:42 +0800

    add hosts filesystem settings

--- hosts/cl-server/hardware.nix
+++ hosts/cl-server/hardware.nix
@@ -12,6 +12,40 @@
   hardware.enableRedistributableFirmware = true;
 
   fileSystems."/".options = [ "size=16g" ];
+  fileSystems."/boot" = {
+    label = "BOOT";
+    fsType = "vfat";
+    options = [
+      "rw"
+      "sync"
+      "nosuid"
+      "nodev"
+      "noexec"
+      "uid=0"
+      "gid=0"
+      "umask=077"
+      "utf8"
+      "errors=remount-ro"
+    ];
+    neededForBoot = false;
+  };
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+    memoryPercent = 100;
+    writebackDevice = "/dev/disk/by-label/swap";
+  };
+  fileSystems."/nix" = {
+    label = "nix";
+    fsType = "ext4";
+    options = [ "defaults" ];
+    neededForBoot = true;
+  };
+  fileSystems."/mnt" = {
+    label = "data";
+    fsType = "bcachefs";
+    neededForBoot = true;
+  };
 
   nixpkgs.hostPlatform = "x86_64-linux";
 }
