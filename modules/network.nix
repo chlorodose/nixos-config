92e44a7c53467def25fb565f601d183b810f03e7
chlorodose <chlorodose@chlorodose.me>
2025-02-14 16:30:34 +0800

    add network configs

---
+++ modules/network.nix
@@ -0,0 +1,16 @@
+{ lib, ... }:
+{
+  networking.useNetworkd = true;
+  networking.useDHCP = false;
+  networking.nftables.enable = true;
+  networking.firewall.enable = true;
+  services.resolved = {
+    enable = lib.mkDefault true;
+    fallbackDns = [
+      "1.1.1.1"
+      "2606:4700:4700::1111"
+      "223.5.5.5"
+      "2400:3200::1"
+    ];
+  };
+}
