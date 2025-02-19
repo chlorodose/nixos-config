7555f5e8453e3f343266d6aba8bdf936b2582f49
chlorodose <chlorodose@chlorodose.me>
2025-02-19 13:13:51 +0800

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "My Nixos Configuration";
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    preservation = {
+      url = "github:nix-community/preservation/main";
+    };
+  };
+  outputs =
+    { self, ... }:
+    let
+      inherit (self) inputs outputs;
+      specialArgs = {
+        inherit inputs outputs;
+      };
+    in
+    {
+      lib = (import ./lib) inputs.nixpkgs.lib;
+      nixosModule = import ./modules/nixos;
+      homeModule = import ./modules/home;
+      nixosConfigurations = { };
+      homeConfigurations = { };
+    };
+}
