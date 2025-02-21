ff919df00258d22a7d7847669132599082323f94
chlorodose <chlorodose@chlorodose.me>
2025-02-21 11:26:38 +0800

    clearify trusted keys

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -67,7 +67,8 @@
           "https://nix-community.cachix.org"
           "https://cache.nixos.org"
         ];
-        trusted-public-keys = [
+        trusted-public-keys = lib.mkForce [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         ];
         auto-optimise-store = true;
