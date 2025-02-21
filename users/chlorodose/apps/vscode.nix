be79218d26b8f00ce8a679aec2909aeefe9a2747
chlorodose <chlorodose@chlorodose.me>
2025-02-21 20:43:15 +0800

    enable vscode wayland support

--- users/chlorodose/apps/vscode.nix
+++ users/chlorodose/apps/vscode.nix
@@ -8,7 +8,9 @@
   config = lib.mkIf config.modules.desktop.enable {
     programs.vscode = {
       enable = true;
-      package = pkgs.vscodium;
+      package = pkgs.vscodium.override {
+        commandLineArgs = "--ozone-platform=wayland";
+      };
     };
   };
 }
