26c6006c4034889b92b50062b6ca1d9d6f8bc14e
chlorodose <chlorodose@chlorodose.me>
2025-02-26 15:39:37 +0800

    fix shit

--- modules/nixos/machine/remote.nix
+++ modules/nixos/machine/remote.nix
@@ -1,6 +1,6 @@
 { config, lib, ... }:
 {
-  options.modules.remote.enable = lib.mkEnableOption "systemd-boot";
+  options.modules.remote.enable = lib.mkEnableOption "remote access";
   config = lib.mkIf config.modules.systemd-boot.enable {
     modules.machine.enable = true;
     services.openssh.settings.DenyUsers = null;
