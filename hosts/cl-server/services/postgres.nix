c944d5daad3bbb51704d2082aa9d9cca251d5a5e
chlorodose <chlorodose@chlorodose.me>
2025-03-08 19:40:50 +0800

    remove ensure user

--- hosts/cl-server/services/postgres.nix
+++ hosts/cl-server/services/postgres.nix
@@ -7,15 +7,6 @@
     dataDir = "/srv/postgresql";
     authentication = lib.mkForce ''
       local all all   peer
-      host  all admin 192.168.0.0/24  password
-      host  all admin 192.168.1.0/24  password
     '';
-    ensureUsers = [
-      {
-        name = "root";
-        ensureClauses.superuser = true;
-        ensureClauses.login = true;
-      }
-    ];
   };
 }
