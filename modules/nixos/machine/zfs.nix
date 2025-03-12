7e46cb5503ad27c194360ea5939f1b3a9618c257
chlorodose <chlorodose@chlorodose.me>
2025-03-12 11:53:16 +0800

    add zfd kernel version

---
+++ modules/nixos/machine/zfs.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+{
+  nixpkgs.overlays = [
+    (final: prev: {
+      linuxKernel_latest_zfs =
+        let
+          zfsCompatibleKernelPackages = lib.filterAttrs (
+            name: kernelPackages:
+            (builtins.match "linux_[0-9]+_[0-9]+" name) != null
+            && (builtins.tryEval kernelPackages).success
+            && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
+          ) final.linuxKernel.packages;
+        in
+        lib.last (
+          lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
+            builtins.attrValues zfsCompatibleKernelPackages
+          )
+        );
+    })
+  ];
+}
