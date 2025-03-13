707695c1e419b508eb04de420bd60c81ed1d486b
chlorodose <chlorodose@chlorodose.me>
2025-03-14 07:48:01 +0800

    use maple font&fix font

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -13,14 +13,42 @@
     networking.networkmanager.enable = true;
     environment.systemPackages = with pkgs; [ wireguard-tools ];
     boot.kernelPackages = lib.mkForce pkgs.linuxPackages_zen;
-    fonts.packages = with pkgs; [
-      material-design-icons
-      font-awesome
-      noto-fonts-emoji
-      nerd-fonts.symbols-only
-      wqy_zenhei
-      fira-code
-    ];
+    fonts = {
+      packages = with pkgs; [
+        material-design-icons
+        font-awesome
+        noto-fonts-emoji
+        (maple-mono.overrideAttrs rec {
+          pname = "MapleMono-NF-CN-unhinted";
+          version = "7.0-beta36";
+          src = fetchurl {
+            url = "https://github.com/subframe7536/Maple-font/releases/download/v${version}/${pname}.zip";
+            sha256 = "sha256-xfkDzhuYzUb9fSLmYVjDMxiZUU6wR/RKVcEKDFDkj4g=";
+          };
+        })
+      ];
+      fontconfig = {
+        hinting.enable = false;
+        defaultFonts = {
+          monospace = [
+            "Maple Mono NF CN"
+            "Font Awesome 6 Free"
+            "Material Design Icons"
+          ];
+          sansSerif = [
+            "Maple Mono NF CN"
+            "Font Awesome 6 Free"
+            "Material Design Icons"
+          ];
+          serif = [
+            "Maple Mono NF CN"
+            "Font Awesome 6 Free"
+            "Material Design Icons"
+          ];
+          emoji = [ "Noto Color Emoji" ];
+        };
+      };
+    };
     services.pipewire = {
       enable = true;
       alsa.enable = true;
