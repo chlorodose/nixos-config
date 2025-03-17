ccfe22f585d61dae2f7f85149a2a133057d8215e
chlorodose <chlorodose@chlorodose.me>
2025-03-17 08:02:26 +0800

    add smaba

---
+++ hosts/cl-server/services/samba.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  pkgs,
+  outputs,
+  ...
+}:
+{
+  users.users.chlorodose = { };
+  users.users."250991817" = {
+    isNormalUser = true;
+    useDefaultShell = true;
+    createHome = false;
+    home = "/var/empty";
+  };
+  sops.secrets."samba" = {
+    sopsFile = outputs.lib.getSecret "services.yaml";
+    mode = "400";
+    owner = "root";
+  };
+  systemd.services."samba-smbd" = {
+    preStart = ''
+      ${pkgs.coreutils}/bin/mkdir -p /var/lib/samba/private &&
+      ${pkgs.samba}/bin/pdbedit -i smbpasswd:${
+        config.sops.secrets."samba".path
+      } -e tdbsam:/var/lib/samba/private/passdb.tdb
+    '';
+  };
+  services.samba = {
+    enable = true;
+    settings = {
+      global = {
+        "passwd program" = "";
+      };
+      homes = {
+        path = "/srv/share/%S";
+        browseable = true;
+        "read only" = false;
+      };
+    };
+  };
+}
