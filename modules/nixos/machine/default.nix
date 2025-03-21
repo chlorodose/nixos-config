f796bbcde328d0f1e877aed506625ed30bfd6423
chlorodose <chlorodose@chlorodose.me>
2025-03-22 07:12:28 +0800

    add smartd

--- modules/nixos/machine/default.nix
+++ modules/nixos/machine/default.nix
@@ -24,5 +24,10 @@
     sops.secrets."user-passwords/root" = {
       neededForUsers = true;
     };
+
+    services.smartd = {
+      enable = true;
+      defaults.monitored = "-a -o on -s (S/../.././02|L/../../7/04)";
+    };
   };
 }
