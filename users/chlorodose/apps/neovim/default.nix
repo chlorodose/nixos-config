9f02e3ab1393f841ecf77cd482f47bd460b7b944
chlorodose <chlorodose@chlorodose.me>
2025-06-07 15:07:03 +0800

    add nvim as pager

--- users/chlorodose/apps/neovim/default.nix
+++ users/chlorodose/apps/neovim/default.nix
@@ -1,7 +1,9 @@
 {
+  pkgs,
   lib,
   inputs,
   outputs,
+  config,
   ...
 }:
 {
@@ -13,15 +15,36 @@
       {
         enable = true;
         defaultEditor = true;
-        viAlias = true;
-        vimAlias = true;
         withNodeJs = true;
         withRuby = false;
         impureRtp = false;
         wrapRc = true;
         nixpkgs.useGlobalPackages = true;
       }
+      # Pager
+      {
+        extraPlugins = [
+          pkgs.vimPlugins.vim-plugin-AnsiEsc
+        ];
+        extraConfigLua = "
+          if vim.g.is_pager then
+            vim.api.nvim_create_autocmd('VimEnter', {
+              pattern = '*',
+              once = true,
+              callback = function()
+                vim.bo.buftype = 'nofile'
+                vim.bo.bufhidden = 'wipe'
+
+                vim.keymap.set('n', 'q', ':q<CR>', { silent = true, buffer = true })
+
+                vim.cmd('AnsiEsc')
+              end,
+            })
+          end
+        ";
+      }
     ]
     ++ (lib.map import (outputs.lib.scanPath ./.))
   );
+  home.sessionVariables.PAGER = "nvim -R -n --cmd 'lua vim.g.is_pager=true'";
 }
