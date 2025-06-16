d28f41ded4d1356b880186e5fc5e79bad06c0645
chlorodose <chlorodose@chlorodose.me>
2025-06-16 14:14:52 +0800

    add browser

---
+++ users/chlorodose/apps/browser.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
+  config = lib.mkIf config.modules.desktop.enable {
+    programs.chromium = {
+      enable = true;
+      package = pkgs.chromium;
+      extensions = [
+        { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # uBlock Origin
+        { id = "bpoadfkcbjbfhfodiogcnhhhpibjhbnh"; } # 沉浸式翻译
+        { id = "doojmbjmlfjjnbmnoijecmcbfeoakpjm"; } # No Script
+        { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # Dark Reader
+        { id = "jfedfbgedapdagkghmgibemcoggfppbb"; } # 猫抓
+        { id = "jpbjcnkcffbooppibceonlgknpkniiff"; } # Global Speed
+        { id = "aeblfdkhhhdcdjpifhhbdiojplfjncoa"; } # 1Password
+      ];
+      commandLineArgs = [
+        "--ozone-platform-hint=auto"
+      ];
+    };
+  };
+}
