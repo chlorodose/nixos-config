c1d656230aa8ed3eaebbdea4d95931201f750420
chlorodose <chlorodose@chlorodose.me>
2025-07-02 08:05:12 +0800

    fix prometheus data

--- hosts/cl-server/services/prometheus.nix
+++ hosts/cl-server/services/prometheus.nix
@@ -281,6 +281,7 @@
       }
     ];
   };
+  system.preserve.directories = ["/var/lib/${config.services.prometheus.stateDir}"];
   services.nginx.upstreams.prometheus = {
     servers = {
       "${config.services.prometheus.listenAddress}:${builtins.toString config.services.prometheus.port}" =
