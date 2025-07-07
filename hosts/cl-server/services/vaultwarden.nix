0205c6d1504bdbe48ec383d20c953b30490db210
chlorodose <chlorodose@chlorodose.me>
2025-07-07 23:15:52 +0800

    setup vaultwarden backup

--- hosts/cl-server/services/vaultwarden.nix
+++ hosts/cl-server/services/vaultwarden.nix
@@ -1,4 +1,4 @@
-{ config, outputs, ... }:
+{ pkgs, config, outputs, ... }:
 {
   sops.secrets."vaultwarden" = {
     sopsFile = outputs.lib.getSecret "services.yaml";
@@ -17,7 +17,7 @@
     config = {
       ROCKET_ADDRESS = "127.0.0.1";
       ROCKET_PORT = 8222;
-      DATA_FOLDER = "/var/lib/vaultwarden/data";
+      DATA_FOLDER = "/var/lib/vaultwarden";
       DATABASE_URL = "postgresql://vaultwarden@/vaultwarden";
       ENABLE_DB_WAL = true;
       DATABASE_MAX_CONNS = 128;
@@ -49,11 +49,50 @@
       EXPERIMENTAL_CLIENT_FEATURE_FLAGS = "inline-menu-positioning-improvements,inline-menu-totp,export-attachments";
       PUSH_RELAY_URI = "https://push.bitwarden.com";
       PUSH_IDENTITY_URI = "https://identity.bitwarden.com";
-      HTTP_PROXY="http://127.0.0.1:7890";
-      HTTPS_PROXY="http://127.0.0.1:7890";
+      HTTP_PROXY = "http://127.0.0.1:7890";
+      HTTPS_PROXY = "http://127.0.0.1:7890";
     };
   };
-  system.preserve.directories = [ "/var/lib/vaultwarden" ];
+  systemd.services.vaultwarden-backup = {
+    path = [
+      pkgs.coreutils
+      pkgs.gnutar
+      pkgs.zstd
+      config.services.postgresql.package
+    ];
+    serviceConfig.Slice = config.systemd.slices.system-vaultwarden.name;
+    requires = [ "postgresql.service" ];
+    startAt = "*-*-* 04:15:00";
+    script = ''
+      set -e -o pipefail
+      umask 0077;
+
+      DATE=$(date +%F)
+      printf "Today is $DATE\n\n"
+
+      printf "Starting dump database...\n"
+      pg_dump --dbname=vaultwarden --no-password --jobs=$(nproc) --no-sync --format=d --file=/tmp/database
+      printf "Database dump success...\n\n"
+
+      printf "Staring creating archive...\n"
+      tar --zstd -cf /srv/backup/vaultwarden/$DATE.tar.zst.tmp -C /tmp database -C /var/lib/vaultwarden attachments -C /var/lib/vaultwarden rsa_key.pem
+      sync /srv/backup/vaultwarden/$DATE.tar.zst.tmp
+      printf "Create archive success...\n\n"
+
+      printf "Finalizing backup...\n"
+      mv /srv/backup/vaultwarden/$DATE.tar.zst.tmp /srv/backup/vaultwarden/$DATE.tar.zst
+      sync /srv/backup/vaultwarden/$DATE.tar.zst
+      rm -rf /tmp/database
+      printf "Backup finished!\n"
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "vaultwarden";
+      Group = "vaultwarden";
+      PrivateTmp = true;
+    };
+  };
+  system.preserve.directories = [ config.services.vaultwarden.config.DATA_FOLDER ];
   systemd.services.vaultwarden.serviceConfig.Slice = config.systemd.slices.system-vaultwarden.name;
   services.nginx.upstreams.vaultwarden = {
     servers = {
