93d7790cc50148c79abb50a0c960dff7c494b76a
chlorodose <chlorodose@chlorodose.me>
2025-08-04 13:57:23 +0800

    fix zed

--- users/chlorodose/apps/zed.nix
+++ users/chlorodose/apps/zed.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   lib,
   config,
   ...
@@ -17,6 +16,35 @@
         "make"
         "basher"
       ];
+      userSettings = {
+        vim_mode = true;
+        features = {
+          copilot = true;
+          edit_prediction_provider = "copilot";
+        };
+        languages = {
+          Nix = {
+            language_servers = [
+              "nixd"
+              "!nil"
+            ];
+          };
+        };
+        icon_theme = "Catppuccin Mocha";
+        base_keymap = "VSCode";
+        theme = "Catppuccin Mocha (Blur)";
+        autosave = "on_focus_change";
+        auto_update = false;
+        buffer_font_family = "Maple Mono NF CN";
+        minimap.show = "auto";
+        tabs = {
+          file_icons = true;
+          git_status = true;
+          show_diagnostics = "all";
+        };
+        toolbar.code_actions = true;
+        diagnostics.inline.enabled = true;
+      };
     };
   };
 }
